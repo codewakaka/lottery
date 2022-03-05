8d60ab32085d05ddc6676c3c8da3820b82a1118f
xgh <1371818484@qq.com>
2022-03-05 20:43:49 +0800

    feat:添加注释

--- lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/service/draw/IDrawExec.java
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/service/draw/IDrawExec.java
@@ -4,7 +4,15 @@ package cn.itedus.lottery.domain.strategy.service.draw;
 import cn.itedus.lottery.domain.strategy.model.res.DrawResult;
 import cn.itedus.lottery.domain.strategy.model.req.DrawReq;
 
+/**
+ * 抽奖执行接口
+ */
 public interface IDrawExec {
 
+    /**
+     * 抽奖方式
+     * @param req 抽奖参数:用户id 策略id
+     * @return 中奖结果
+     */
     DrawResult doDrawExec(DrawReq req);
 }
